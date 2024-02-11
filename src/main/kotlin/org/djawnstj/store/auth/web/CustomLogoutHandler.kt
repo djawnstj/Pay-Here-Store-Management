5a225c0f2b4ff2d2ad101e3253be5d623c1a5ff8
junseo <djawnstj44@naver.com>
2024-02-11 23:36:05 +0900

    Refactor: CustomLogoutHandler 예외 핸들링

--- src/main/kotlin/org/djawnstj/store/auth/web/CustomLogoutHandler.kt
+++ src/main/kotlin/org/djawnstj/store/auth/web/CustomLogoutHandler.kt
@@ -1,11 +1,14 @@
 package org.djawnstj.store.auth.web
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.djawnstj.store.auth.repository.TokenRepository
 import org.djawnstj.store.auth.service.JwtService
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.djawnstj.store.common.api.Response
 import org.djawnstj.store.common.exception.ErrorCode
 import org.djawnstj.store.common.exception.GlobalException
+import org.springframework.http.MediaType
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.logout.LogoutHandler
@@ -14,7 +17,8 @@ import org.springframework.stereotype.Service
 @Service
 class CustomLogoutHandler(
     private val jwtService: JwtService,
-    private val tokenRepository: TokenRepository
+    private val tokenRepository: TokenRepository,
+    private val objectMapper: ObjectMapper
 ): LogoutHandler {
 
     override fun logout(request: HttpServletRequest?, response: HttpServletResponse?, authentication: Authentication?) {
@@ -27,7 +31,17 @@ class CustomLogoutHandler(
         tokenRepository.findByJti(jti)?.let {
             tokenRepository.deleteByJti(jti)
             SecurityContextHolder.clearContext()
-        } ?: throw GlobalException(ErrorCode.TOKEN_NOT_FOUND)
+        } ?: run {
+            GlobalException(ErrorCode.TOKEN_NOT_FOUND).let { ex ->
+                val responseEntity = Response.error(ex.errorCode, ex.message)
+                val jsonResponse = objectMapper.writeValueAsString(responseEntity)
+
+                response?.contentType = "application/json"
+                response?.characterEncoding = "UTF-8"
+                response?.status = ex.errorCode.status.value()
+                response?.writer?.write(jsonResponse)
+            }
+        }
     }
 
 }
